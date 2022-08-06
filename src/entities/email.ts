7183841883a692ef5c5cc3ea68b3d81847a6aac7
caique.moraes <caique.moraes@compasso.com.br>
2022-08-06 09:52:55 -0300

    feat: validate emails domain parts larger than 63 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -5,7 +5,10 @@ export class Email {
 
     const [local, domain] = email.split('@')
     if (local.length > 64 || local.length === 0) return false
-    if (domain.length > 255) return false
+    if (domain.length > 255 || domain.length === 0) return false
+
+    const domainParts = domain.split('.')
+    if (domainParts.some((part) => part.length > 63)) return false
 
     return true
   }
