f2465585921a18700305ed65200b530437b19a49
caique.moraes <caique.moraes@compasso.com.br>
2022-08-06 15:54:16 -0300

    test: register user on mailing list

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
 import { UserData } from '../../../entities/user-data'
-import { InMemoryUserRepository } from './in-memoery-user-repository'
+import { InMemoryUserRepository } from './in-memory-user-repository'
 
 describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
