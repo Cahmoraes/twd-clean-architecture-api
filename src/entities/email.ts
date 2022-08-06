15973db7674b26f9aa2f2e599ebcd23994f087d2
caique.moraes <caique.moraes@compasso.com.br>
2022-08-06 13:50:48 -0300

    feat: either type and right and left classes

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,21 @@
+import { Either, left, right } from '../shared/either'
+import { InvalidEmailError } from './errors/invalid-email-error'
+
 export class Email {
+  private readonly email: string
+
+  constructor (email: string) {
+    this.email = email
+  }
+
+  static create (email: string): Either<InvalidEmailError, Email> {
+    if (this.validate(email)) {
+      return right(new Email(email))
+    }
+
+    return left(new InvalidEmailError())
+  }
+
   static validate (email: string): boolean {
     if (!email) return false
     if (email.length > 320) return false
