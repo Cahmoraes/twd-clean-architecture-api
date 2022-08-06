9e33dff722d7f4c3e623efe4b9f09b69f87dc584
caique.moraes <caique.moraes@compasso.com.br>
2022-08-06 09:44:28 -0300

    feat: validate domain is larger than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,13 +1,11 @@
 export class Email {
   static validate (email: string): boolean {
     if (!email) return false
+    if (email.length > 320) return false
 
-    if (email.length > 320) {
-      return false
-    }
-
-    const [local] = email.split('@')
+    const [local, domain] = email.split('@')
     if (local.length > 64) return false
+    if (domain.length > 255) return false
 
     return true
   }
