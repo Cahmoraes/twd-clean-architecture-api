7147f2a84c3aface5c095eb745ee1f6926f65206
caique.moraes <caique.moraes@compasso.com.br>
2022-08-06 16:02:53 -0300

    test: prevent to add user with invalid name

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
 import { UserData } from '../../entities/user-data'
 import { left } from '../../shared/either'
 import { UserRepository } from '../ports/user-repository'
@@ -30,5 +31,18 @@ describe('Register user on mailing list use case', () => {
 
     expect(user).toBeNull()
     expect(response).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not add user with invalid name', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const invalidName = ''
+    const validEmail = 'any@mail.com'
+    const response = await useCase.registerUserOnMailingList({ name: invalidName, email: validEmail })
+    const user = await repo.findUserByEmail('any@email.com')
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidNameError()))
   })
 })
